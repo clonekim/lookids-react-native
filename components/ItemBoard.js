5ea32ceb35b5b6bf5b15985b596d3b08cc50f56f
daehee.kim <dh.kim@eastend.co.kr>
2018-11-23 17:55:30 +0900

    아이템카드에 랭크, 폰트 수정

--- components/ItemBoard.js
+++ components/ItemBoard.js
@@ -3,6 +3,7 @@ import {Image, ImageBackground, View, Text, TouchableOpacity } from 'react-nativ
 import Masonry from 'react-native-masonry-layout';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import SearchBox from '../components/SearchBox';
+import ItemCard from './ItemCard';
 
 export default class Item extends Component {
 
@@ -12,13 +13,12 @@ export default class Item extends Component {
 
   componentDidMount() {
     const images =[
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_2.jpg', msg: '입으면 이쁘고 이쁘고 이쁘고 이쁘서 안사면 후회할 거야 ㅋㅋㅋㅋㅋ - 가방',  key: '1' },
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_20.jpg', msg: 'ahahaha',  key: '2' },
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_21.jpg', msg: 'This is awesome project', key: '3'},
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_12.jpg', msg:'010012323',  key: '4' },
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_22.jpg', msg: 'xcc',  key: '5'},
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_8.jpg', msg: '뭐지??', key: '6'},
-      { uri: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_6.jpg', msg: '시계', key: '7'},
+      { image_path: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_2.jpg', rank:1,   name: '빌티드 레더 퍼 무스탕', favor: 1, price:45500,  key: '1' },
+      { image_path: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_20.jpg', rank: 2, name: '모모 니트 샤 우너피스',favor: 1, price:29000,  key: '2' },
+      { image_path: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_21.jpg', rank: 3, name: '남아 코드 특가', favor:0, price: 19800, key: '3'},
+      { image_path: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_12.jpg', rank: 4, name:'아이 하루 롱패팅 6색', favor:0, price:47000,  key: '4' },
+      { image_path: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_22.jpg', rank: 5,  name: 'SNAZZY TRACKSUITSET', favor: 1, price:89000,  key: '5'},
+      { image_path: 'http://s3-ap-northeast-2.amazonaws.com/demo-lookids/1/item_8.jpg', rank: 6, name: '양털 코트(공용)', price: 67000, key: '6'},
     ];
 
     this.refs.list.addItems(images);
@@ -39,41 +39,7 @@ export default class Item extends Component {
             paddingLeft: 2,
             paddingBottom: 2            
           }}
-          renderItem={item => {
-            return (
-              <View style={{
-                      margin: 2,
-                      padding: 8,
-                      borderRadius: 2,
-                      overflow: 'hidden',
-                      borderWidth: 1,
-                      borderColor: '#dedede',
-                    }}>
-
-                <View style={{alignItems:'center'}}>
-                  <TouchableOpacity style={{width:113, height:130}}>
-                    <Image source={{uri: item.uri }} style={{position:'absolute', left:0, top:0, width:113, height: 130}}/>
-                  </TouchableOpacity>
-                </View>
-                
-                <View style={{padding:2}}>
-                  <Text style={{fontSize: 12, fontWeight:'400'}}>{item.msg}</Text>
-                  <Text style={{fontSize: 12, fontWeight:'700'}}>67,000원</Text>
-                </View>
-
-                <TouchableOpacity>
-                  <View style={{flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center', borderTopWidth: 1, borderColor:'#efefef'}}>
-                    <Icon name="star" size={16} style={{ marginTop: 4, marginLeft:4, color:'yellow' }} />
-                    <Text style={{fontSize: 12, paddingLeft: 5}}>찜</Text>
-                  </View>                  
-                </TouchableOpacity>
-
-                <Icon name="star" size={23} style={{position: 'absolute', marginTop: 4, marginLeft:4, color:'yellow' }} />
-                <Text style={{position:'absolute', fontWeight:'700', color:'red', marginTop: 6, marginLeft: 10}} >1</Text>
-
-              </View>
-            );} 
-          }/>
+          renderItem={item => <ItemCard item={item} />}/>
 
       </View>
       
--- components/ItemCard.js
+++ components/ItemCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import util from '../util';
 
 
-const ItemCard  = (item) = (
+const ItemCard  = (props) => (
   <View style={{
           overflow: 'hidden',
           width: 120,
@@ -15,23 +17,26 @@ const ItemCard  = (item) = (
 
     <View style={{alignItems:'center'}}>
       <TouchableOpacity style={{width:120, height:130}} onPress={()=> NavigationService.navigate('ItemWebPage', item)}>
-        <Image source={{uri: item.image_path }} style={{position:'absolute', left:0, top:0, width:120, height: 130}}/>
+        <Image source={{uri: props.item.image_path }} style={{position:'absolute', left:0, top:0, width:120, height: 130}}/>
       </TouchableOpacity>
     </View>
     
     <View style={{padding:2, alignItems:'flex-start'}}>
-      <Text style={{fontSize: 10, color:'#495057', fontFamily:'Noto Sans CJK KR Regular', fontWeight:'400'}}>{item.name}</Text>
-      <Text style={{fontSize: 10, color:'#343a40', fontFamily:'Noto Sans CJK KR Bold',  fontWeight:'700'}}>{util.currencyFormat(item.price)}원</Text>
+      <Text style={{fontSize: 10, color:'#495057', fontFamily:'Noto Sans CJK KR Regular', fontWeight:'400'}}>{props.item.name}</Text>
+      <Text style={{fontSize: 10, color:'#343a40', fontFamily:'Noto Sans CJK KR Bold',  fontWeight:'700'}}>{util.currencyFormat(props.item.price)}원</Text>
     </View>
 
     <View style={{flex:1, height:0, borderWidth:1, borderColor:'#f9f9fa'}}></View>
 
     <TouchableOpacity style={{height:27}}>
       <View style={{flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center', borderTopWidth: 1, borderColor:'#efefef'}}>
+        <Icon name="star" size={11} color={props.item.favor ==1 ? '#ffaf08':'#ffffff'} />
         <Text style={{fontSize: 10, color:'#868e96', fontFamily:'Noto Sans CJK KR Regular', fontWeight:'400',  paddingLeft: 5}}>찜</Text>
       </View>                  
     </TouchableOpacity>
 
+    <Image source={require('../assets/mark.png')} style={{width:16, height:23, position:'absolute',}} />
+    <Text style={{position:'absolute', color:'#fff', fontSize:10, fontWeight:'400',}}>{props.item.rank}</Text>
   </View>
 );
 
